93257b88455cddc7b5b968518cd8d67ea3f21770
Aset Riza <asetriza@Asets-MacBook-Pro.local>
2021-05-25 10:29:35 +0600

    Renamed transport

---
+++ internal/transport/rest/v1/response.go
@@ -0,0 +1,13 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorResponce struct {
+	Message string `json:"message"`
+}
+
+func newErrorResponce(c *gin.Context, statusCode int, message string) {
+	c.AbortWithStatusJSON(statusCode, ErrorResponce{message})
+}
